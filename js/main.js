5c6084457f958375c0a2f7080e50ba0abf0611a9
Hamzh Freijat <hamzhg2016@gmail.com>
2022-01-25 22:14:00 +0200

    event

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+let refId = 10000;
+const uniqueid = function(){
+    refId++;
+    return refId;
+}
+//
 function randomSalary(level){
     let max ;
     let min ;
@@ -11,7 +17,7 @@ function randomSalary(level){
         max = 1000
         min = 500
     }
-    return Math.floor(Math.random() * (max - min)) + min +100;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 function netSalary(tax , level){
     salary = randomSalary(level)
@@ -26,29 +32,63 @@ function Employee(id ,fullName,dapartment,level,imageUrl,salary){
     this.imageUrl = imageUrl;
     this.salary = salary;
 }
+//
+// const ghazi = new Employee(1000 , "Ghazi Samer" , "Administration" , "Senior");
+// const lana = new Employee(1001 , "Lana Ali" , "Finance" , "Senior");
+// const tamara = new Employee(1002 , "Tamara Ayoub" , "Marketing" , "Senior");
+// const safi = new Employee(1003 , "Safi Walid" , "Administration" , "Mid-Senior");
+// const omar = new Employee(1004 , "Omar Zaid" , "Development" , "Senior");
+// const rana = new Employee(1005 , "Rana Saleh" , "Development" , "Junior");
+// const hadi = new Employee(1006 , "Hadi Ahmad" , "Finance" , "Mid-Senior");
+//
+// const employeeArray = [ghazi , lana , tamara  , safi , omar , rana , hadi];
+//
+// Employee.prototype.render = function (){
+//     const tableBody =  document.getElementById('user-table-body');
+//     let counter = 0 ;
+//     employeeArray.forEach(item => {
+//         counter++;
+//         tableBody.innerHTML +=
+//             `<tr><th>${counter}</th>
+//              <td>${item.fullName}</td>
+//               <td>${netSalary(0.075 , item.level)}</td>
+//              </tr>
+//             `
+//     });
+// };
+//
+// ghazi.render();
 
-const ghazi = new Employee(1000 , "Ghazi Samer" , "Administration" , "Senior");
-const lana = new Employee(1001 , "Lana Ali" , "Finance" , "Senior");
-const tamara = new Employee(1002 , "Tamara Ayoub" , "Marketing" , "Senior");
-const safi = new Employee(1003 , "Safi Walid" , "Administration" , "Mid-Senior");
-const omar = new Employee(1004 , "Omar Zaid" , "Development" , "Senior");
-const rana = new Employee(1005 , "Rana Saleh" , "Development" , "Junior");
-const hadi = new Employee(1006 , "Hadi Ahmad" , "Finance" , "Mid-Senior");
-
-const employeearry = [ghazi , lana , tamara  , safi , omar , rana , hadi];
-
-Employee.render = function (){
-    const tableBody =  document.getElementById('user-table-body');
-    let counter = 0 ;
-
-    employeearry.forEach(item => {
-        counter++;
-        tableBody.innerHTML +=
-            `<tr><th>${counter}</th>
-             <td>${item.fullName}</td>
-              <td>${netSalary(0.075 , item.level)}</td>
-             </tr>
-            `
-    });
-};
-const hamzh = Employee.render();
+const employeeForm = document.getElementById('create-employee-form');
+employeeForm.addEventListener('submit' ,(e) => {
+    e.preventDefault();
+    const userId = uniqueid();
+    const userFullName = e.target.name.value;
+    const userDapartment = e.target.department.value;
+    const userLevel = e.target.level.value;
+    const userImageUrl = e.target.imageUrl.value;
+    const userSalary = netSalary(0.075 , userLevel)
+    let newEmployee = new Employee(userId, userFullName ,userDapartment, userLevel,userImageUrl,userSalary);
+    Employee.prototype.render = function (){
+        const cardSection = document.getElementById("emploee-card");
+        const div = document.createElement('div');
+        const img = document.createElement('img');
+        const h5 = document.createElement('h5');
+        const p1 = document.createElement('p');
+        const p2 = document.createElement('p');
+        div.setAttribute('class' , 'card')
+        img.setAttribute('src' , userImageUrl);
+        img.setAttribute('alt' , 'card-image');
+        h5.textContent = `Dapartment: ${userDapartment} - level :${userLevel}`;
+        p1.textContent = `Name: ${userFullName} -ID: ${userId}`;
+        p1.setAttribute('class' , 'card-text');
+        p2.textContent = `${userSalary}`;
+        p2.setAttribute('class' , 'card-salary');
+        cardSection.appendChild(div);
+        div.appendChild(img);
+        div.appendChild(h5);
+        div.appendChild(p1);
+        div.appendChild(p2);
+    }
+    newEmployee.render();
+})
